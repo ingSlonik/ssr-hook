c7fb23945601ff0f24cf27843089519d2e8e26b3
Filip Paulů <ing.fenix@seznam.cz>
2024-12-05 15:33:46 +0100

    Remove CLI text files

--- muj-project/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { RouterProvider } from "easy-page-router/react";
-
-import Router from "./Router";
-
-export default function App() {
-    return <RouterProvider>
-        <Router />
-    </RouterProvider>;
-}
